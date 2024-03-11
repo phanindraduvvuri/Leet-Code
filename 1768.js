ca36d71fc2dd89a7fbcf06c7fe44239f057ce2d5
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-03-12 01:02:28 +0530

    Updated implementation to optimize.

--- 1768.js
+++ 1768.js
@@ -4,29 +4,29 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-  let comb_arr = [];
+  let comb_arr = "";
   let i = 0,
     j = 0;
 
   while (i < word1.length && j < word2.length) {
-    comb_arr.push(word1.charAt(i));
-    comb_arr.push(word2.charAt(j));
+    comb_arr = comb_arr.concat(word1[i]);
+    comb_arr = comb_arr.concat(word2[j]);
 
     i++;
     j++;
   }
 
   while (i < word1.length) {
-    comb_arr.push(word1.charAt(i));
+    comb_arr = comb_arr.concat(word1[i]);
     i++;
   }
 
   while (j < word2.length) {
-    comb_arr.push(word2.charAt(j));
+    comb_arr = comb_arr.concat(word2[j]);
     j++;
   }
 
-  return comb_arr.join("");
+  return comb_arr;
 };
 
 let word1 = "abc";
