f080474dd1aaa877f6e33836a706e1d14f438093
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-05-05 20:56:00 +0530

    Added Solution for Question No. 58

--- 58.js
+++ 58.js
@@ -3,18 +3,8 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-  let inputString = s.trim();
-  let n = inputString.length - 1;
-  let wordLength = 0;
-
-  while (n >= 0) {
-    if (inputString.charAt(n) == " ") break;
-
-    wordLength++;
-    n--;
-  }
-
-  return wordLength;
+  let words = s.trim().split(" ");
+  return words[words.length - 1].length;
 };
 
 const input = "  Hello World  ";
