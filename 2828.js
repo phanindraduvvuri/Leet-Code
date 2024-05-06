0ad69c52c9ca16871bd1d894f5fa3ab34257467b
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-05-06 22:14:27 +0530

    Added solution for #2828

---
+++ 2828.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function (words, s) {
+  return words.map((word) => word[0]).reduce((a, b) => a + b) == s;
+};
+
+let testCases = [
+  [["alice", "bob", "charlie"], "abc"],
+  [["an", "apple"], "a"],
+  [["never", "gonna", "give", "up", "on", "you"], "ngguoy"],
+];
+
+for (let i = 0; i < testCases.length; i++) {
+  console.log(isAcronym(testCases[i][0], testCases[i][1]));
+}
