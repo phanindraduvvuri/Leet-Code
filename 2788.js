3093691fa1acc5afeea5d0312971c21646414aa7
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-05-07 08:58:33 +0530

    Solution for #2788

---
+++ 2788.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function (words, separator) {
+  let res = [];
+  words.map((word) => res.push(...word.split(separator)));
+
+  return res.filter((word) => word.length > 0);
+};
+
+let testCases = [
+  [["one.two.three", "four.five", "six"], "."],
+  [["$easy$", "$problem$"], "$"],
+  [["|||"], "|"],
+];
+
+for (let i = 0; i < testCases.length; i++) {
+  console.log(splitWordsBySeparator(testCases[i][0], testCases[i][1]));
+}
