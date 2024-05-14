50875ad0fcc82588069ad8a221ce614a02eb24f6
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-05-14 21:07:20 +0530

    Added Solution for #1528

---
+++ 1528.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+  let returnStringArray = new Array(s.length);
+
+  for (let i = 0; i < s.length; i++) {
+    returnStringArray[indices[i]] = s.charAt(i);
+  }
+
+  return returnStringArray.join("");
+};
+
+let testCases = [
+  ["codeleet", [4, 5, 6, 7, 0, 2, 1, 3]],
+  ["abc", [0, 1, 2]],
+];
+
+testCases.forEach((testCase) =>
+  console.log(restoreString(testCase[0], testCase[1]))
+);
