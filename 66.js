013416d620ce62876808c762372b51816b558985
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-05-14 22:08:22 +0530

    Solution for #66

---
+++ 66.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  let n = digits.length;
+
+  digits[n - 1] += 1;
+  for (let i = n - 1; i >= 1; i--) {
+    if (digits[i] == 10) {
+      digits[i] = 0;
+      digits[i - 1] += 1;
+    }
+  }
+
+  if (digits[0] == 10) {
+    digits.splice(1, 0, 0);
+    digits[0] = 1;
+  }
+
+  return digits;
+};
+
+let testCases = [[1, 2, 3], [4, 3, 2, 1], [9], [1, 9], [9, 9], [9, 9, 9], [0]];
+
+testCases.forEach((testCase) => console.log(plusOne(testCase)));
